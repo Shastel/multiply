27a429d2c6e6a818d8968430293548d60e5251f8
DenisMezhuev <alexmezhuev@yandex.by>
2019-10-02 13:53:01 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function multiply(first, second) {
-  // your solution
+module.exports = function multiply(first = Number(first), second = Number(second)) {
+  return (BigInt(first) * BigInt(second)).toString();
+  
 }
